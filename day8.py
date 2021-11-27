c174ae2143fd59670ded434994b02729ab092a5e
judifer <45107199+judifer@users.noreply.github.com>
2021-11-27 13:33:52 +0100

    created ceasar cipher

---
+++ day8.py
@@ -0,0 +1,52 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+print("Welcome to...\n", logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def base():
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    print(f"Your encrypted word is {dir(direction, text, shift)}")
+    a = input("Type 'yes' if you'd like to encrypt/decrypt another message.\n").lower()
+    if a == "yes":
+        return base()
+    else:
+        exit()
+
+def dir(d, t, s):
+    if d == "decode":
+        s -= (2 * s)
+    return ceasar(t, s)
+
+def ceasar(t, s):
+    shifted_text = ""
+    for a in t:
+        if a not in alphabet:
+            shifted_text += a
+        else:
+            for idx, b in enumerate(alphabet):
+                if a == b:
+                    c = idx + s
+                    if c > (len(alphabet) - 1):
+                        c = c % (len(alphabet))
+                    shifted_text += alphabet[c]
+    return shifted_text
+
+base()
